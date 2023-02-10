44e6a8058d3f0ef496b574f7f7f44bbe9382f56e
UCAPIP <ucapip@gmail.com>
2023-02-10 18:00:48 +0300

    Добавлено скрытие полей

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-
+document.getElementById('output_1').style.visibility = 'hidden';
 
 document.getElementById('kmInput').addEventListener('input', function(e) {
+	document.getElementById('output_1').style.visibility = 'visible';
 	let km = e.target.value;
 	console.log(km);
 	document.getElementById('mOutput').innerHTML = km*1000;
@@ -8,7 +9,9 @@ document.getElementById('kmInput').addEventListener('input', function(e) {
 	document.getElementById('mmOutput').innerHTML = km*1000000000;
 });
 
+document.getElementById('output_2').style.visibility = 'hidden';
 document.getElementById('kgInput').addEventListener('input', function(f) {
+	document.getElementById('output_2').style.visibility = 'visible';
 	let kg = f.target.value;
 	console.log(kg);
 	document.getElementById('gOutput').innerHTML = kg*1000;
